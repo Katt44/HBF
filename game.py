87fded572b456373e16bf012fbd21d32318bcfce
Hackbright Student <no-reply@hackbrightacademy.com>
2017-05-23 16:03:41 -0700

    added range

--- game.py
+++ game.py
@@ -1,11 +1,17 @@
-from random import randint
 """A number-guessing game. """
+
+from random import randint
+
 print "Howdy what's your name?"
+
 user_name = raw_input("type in your name ")
 random_num = randint(1, 100)
 count = 0
+
 while True:
     user_guess = int(raw_input("guess a number between 1 and 100: "))
+    #if user_guess not in range(1, 101):
+        print "Nice try.  Please enter a number between 1 and 100!! \n "
     if user_guess != random_num:
         if user_guess > random_num:
             print "Your number is too high, try again! \n "
