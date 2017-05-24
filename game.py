e72b63ca4834a210bbff01f8dd92bef8f7a3c0c0
Hackbright Student <no-reply@hackbrightacademy.com>
2017-05-23 18:05:06 -0700

    last part needs to be completed see notes at the bottom of subl file

--- game.py
+++ game.py
@@ -2,6 +2,7 @@
 
 from random import randint
 
+
 print "Howdy what's your name?"
 
 user_name = raw_input("type in your name ")
@@ -9,17 +10,25 @@ random_num = randint(1, 100)
 count = 0
 
 while True:
-
+    # user_guess = raw_input("guess a number between 1 and 100: ")
     try:
-        # user_guess = int(raw_input("guess a number between 1 and 100: "))
         user_guess = int(raw_input("guess a number between 1 and 100: "))
+        #fix me complete : If a user enters a decimal, inform the user that you rounded their number. Round the number and keep playing
+        # user_guess = float(raw_input("guess a number between 1 and 100: "))
         
-        #break
+      
     except ValueError:
         print("Heyyyy, put in a N U M B E R! \n ")
         continue
  #if user_guess not in range(1, 101):
        # print "Nice try.  Please enter a number between 1 and 100!! \n "
+    if "." in user_guess:
+        print "You entered a float, we rounded your number."
+        #fixme
+        # user_guess = int(round(float(user_guess)))
+        # user_guess = round(user_guess)
+
+        print user_guess
 
     if user_guess < 1 or user_guess > 100:
         print "Nice try! Try again! "
@@ -37,5 +46,8 @@ while True:
         # print "Congrats! {}! You found my number in {}".format(user_name, count)
         break
 
-
+# to complete  do " If a user enters a decimal, inform the user that you rounded their number. Round the number and keep playing "
+#do:"
 # Put your code here
+# user_guess = raw_input("guess a numb") #  << outside  try
+#user_guess_int = int(round(float(user_guess)))
