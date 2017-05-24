8bce1777a83f8745f03cd4d049b08b0bce8bc3bc
Hackbright Student <no-reply@hackbrightacademy.com>
2017-05-23 17:11:43 -0700

    added try and except

--- game.py
+++ game.py
@@ -9,8 +9,16 @@ random_num = randint(1, 100)
 count = 0
 
 while True:
-    user_guess = int(raw_input("guess a number between 1 and 100: "))
-    #if user_guess not in range(1, 101):
+
+    try:
+        # user_guess = int(raw_input("guess a number between 1 and 100: "))
+        user_guess = int(raw_input("guess a number between 1 and 100: "))
+        
+        #break
+    except ValueError:
+        print("Heyyyy, put in a N U M B E R! \n ")
+        continue
+ #if user_guess not in range(1, 101):
        # print "Nice try.  Please enter a number between 1 and 100!! \n "
 
     if user_guess < 1 or user_guess > 100:
@@ -22,7 +30,7 @@ while True:
             count += 1
         else:
             print "Your number is too low, try again! \n "
-            count += 1
+            count += 1      
 
     else:
         print "Congrats! %s! You found my number in %i tries! " % (user_name, count)
